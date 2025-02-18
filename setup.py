bdb10b25b51a8d60b35b94a03b50de16b1068352
Sachith Rukshan <rukshan.dks@gmail.com>
2025-02-18 15:02:48 +0530

    Update setup.py
    mltest

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ def get_requirements(file_path:str)->List[str]:
     return requirements
 
 setup(
-name='student_performance',
+name='mltest',
 version='0.0.1',
 author='Rukshan',
 author_email='Rukshan.dks@gmail.com',
