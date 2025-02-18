c93435956210f845bec16b2b69e9e9bc5d6cbd7e
Sachith Rukshan <rukshan.dks@gmail.com>
2025-02-18 14:50:00 +0530

    Update setup.py
    test3

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ def get_requirements(file_path:str)->List[str]:
     return requirements
 
 setup(
-name='mlproject',
+name='student_performance',
 version='0.0.1',
 author='Rukshan',
 author_email='Rukshan.dks@gmail.com',
